80a435ffdbeea055c7ef16337c733119870447d2
Robert Memmer <rmemmer3@gmail.com>
2023-08-01 17:51:56 -0400

    API calls for mapping, pricing, and volume sources now work.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,33 @@
-let priceURL = "prices.runescape.wiki/api/v1/osrs"
+let mappingURL = "https://prices.runescape.wiki/api/v1/osrs/mapping"
+let priceURL = "https://prices.runescape.wiki/api/v1/osrs/latest"
 let volURL = "https://api.weirdgloop.org/exchange/history/osrs/latest?id=2"
 
-fetch(url)
+let headers = new Headers({
+    "Accept"       : "application/json",
+    "Content-Type" : "application/json",
+    "User-Agent": "Personal portfolio trading project.",
+    "Email" : "rmemmer3@gmail.com"
+})
+
+fetch(priceURL, {
+    mode: 'cors',
+    method : 'GET',
+    headers : headers
+})
+    .then(res => res.json())
+    .then(dater => console.log(dater))
+
+fetch(mappingURL, {
+    mode: 'cors',
+    method : 'GET',
+    headers : headers
+})
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+fetch(volURL, {
+    method : 'GET',
+    headers : headers
+})
     .then(res => res.json())
     .then(data => console.log(data))
